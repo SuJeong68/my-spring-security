21933dc6bd95ac8e6f4afb0256d629b7a833c28c
sudang <tidh1640@naver.com>
2023-05-18 21:49:39 +0900

    Feat✨: CustomAuthenticationProvider 구현

--- src/main/java/com/sudang/myspringsecurity/config/ProjectConfig.java
+++ src/main/java/com/sudang/myspringsecurity/config/ProjectConfig.java
@@ -1,7 +1,11 @@
 package com.sudang.myspringsecurity.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,7 +18,15 @@ import javax.sql.DataSource;
  * @date   2023/05/14
 **/
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider);
+    }
 
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
--- src/main/java/com/sudang/myspringsecurity/security/CustomAuthenticationProvider.java
+++ src/main/java/com/sudang/myspringsecurity/security/CustomAuthenticationProvider.java
@@ -1,35 +1,55 @@
 package com.sudang.myspringsecurity.security;
 
-import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 
+/**
+ * @author sudang
+ * @date   2023/05/18
+**/
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     // 인증의 전체 논리를 나타냄
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        // Principal 인터페이스의 getName() 메서드를 Authentication 에서 상속받음
-        String username = authentication.getName();
-        String password = String.valueOf(authentication.getCredentials());
 
-        // UserDetailsService 및 PasswordEncoder를 호출해서 사용자 이름과 암호를 테스트
-        if ("sudang".equals(username) && "12345".equals(password)) {
-            return new UsernamePasswordAuthenticationToken(username, password, Arrays.asList());
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        if (passwordEncoder.matches(password, userDetails.getPassword())) {
+            // 암호 일치 시, 세부 정보가 포함된 Authentication 게약의 구현 = UsernamePasswordAuthenticationToken 을 반환
+            return new UsernamePasswordAuthenticationToken(
+                    username,
+                    password,
+                    userDetails.getAuthorities()
+            );
         } else {
-            throw new AuthenticationCredentialsNotFoundException("Error in authentication!");
+            // BadCredentialsException <- AuthenticationException 상속
+            throw new BadCredentialsException("Something went wrong!");
         }
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
-        // Authentication 형식의 구현을 추가함
-        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+        // authentication 이 UsernamePasswordAuthenticationToken 종류의 키를 지원하도록 설정
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
     }
 }
