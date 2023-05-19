1f551972a463ac475a207a7ba0b3a2d7221119ce
sudang <tidh1640@naver.com>
2023-05-19 14:14:46 +0900

    Feat✨: formLogin handler 설정

--- src/main/java/com/sudang/myspringsecurity/config/ProjectConfig.java
+++ src/main/java/com/sudang/myspringsecurity/config/ProjectConfig.java
@@ -2,18 +2,11 @@ package com.sudang.myspringsecurity.config;
 
 import com.sudang.myspringsecurity.security.CustomAuthenticationFailureHandler;
 import com.sudang.myspringsecurity.security.CustomAuthenticationSuccessHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 /**
  * @author sudang
@@ -25,13 +18,13 @@ import javax.sql.DataSource;
 @Configuration
 public class ProjectConfig {
 
-//    private final CustomAuthenticationSuccessHandler authenticationSuccessHandler;
-//    private final CustomAuthenticationFailureHandler authenticationFailureHandler;
-//
-//    public ProjectConfig(CustomAuthenticationSuccessHandler authenticationSuccessHandler, CustomAuthenticationFailureHandler authenticationFailureHandler) {
-//        this.authenticationSuccessHandler = authenticationSuccessHandler;
-//        this.authenticationFailureHandler = authenticationFailureHandler;
-//    }
+    private final CustomAuthenticationSuccessHandler authenticationSuccessHandler;
+    private final CustomAuthenticationFailureHandler authenticationFailureHandler;
+
+    public ProjectConfig(CustomAuthenticationSuccessHandler authenticationSuccessHandler, CustomAuthenticationFailureHandler authenticationFailureHandler) {
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
+        this.authenticationFailureHandler = authenticationFailureHandler;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -39,25 +32,9 @@ public class ProjectConfig {
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
-                .defaultSuccessUrl("/home", true)
+                .successHandler(authenticationSuccessHandler)
+                .failureHandler(authenticationFailureHandler)
                 .and()
             .build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        String usersByUsernameQuery = "SELECT username, password, enabled FROM users WHERE username = ?";
-        String authoritiesByUsernameQuery = "SELECT username, authority FROM spring.authorities WHERE username = ?";
-
-        var userDetailManager = new JdbcUserDetailsManager(dataSource);
-        userDetailManager.setUsersByUsernameQuery(usersByUsernameQuery);
-        userDetailManager.setAuthoritiesByUsernameQuery(authoritiesByUsernameQuery);
-
-        return new JdbcUserDetailsManager(dataSource);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
     }
 }
--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT IGNORE INTO `spring`.`authorities` VALUES (NULL, 'sudang', 'write');
-INSERT IGNORE INTO `spring`.`users` VALUES (NULL, 'sudang', '12345', '1');
