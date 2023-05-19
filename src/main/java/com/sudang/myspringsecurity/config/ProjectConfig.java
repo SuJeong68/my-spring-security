8c77e4e48c80c2aef9ff1186883053a681c07e4e
sudang <tidh1640@naver.com>
2023-05-19 14:02:37 +0900

    Feat✨: EnableWebSecurity 설정 및 formLogin() 사용

--- src/main/java/com/sudang/myspringsecurity/config/ProjectConfig.java
+++ src/main/java/com/sudang/myspringsecurity/config/ProjectConfig.java
@@ -1,14 +1,17 @@
 package com.sudang.myspringsecurity.config;
 
-import com.sudang.myspringsecurity.security.CustomEntryPoint;
+import com.sudang.myspringsecurity.security.CustomAuthenticationFailureHandler;
+import com.sudang.myspringsecurity.security.CustomAuthenticationSuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
@@ -16,17 +19,29 @@ import javax.sql.DataSource;
  * @author sudang
  * @date 2023/05/14
  **/
+// Spring security 5.7 이상에서 WebSecurityConfigurerAdapter 사용을 권장하지 않기 때문에
+// SecurityFilterChain 사용하는 EnableWebSecurity 로 변경
+@EnableWebSecurity(debug = true)
 @Configuration
-public class ProjectConfig extends WebSecurityConfigurerAdapter {
+public class ProjectConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic(c -> {
-            c.realmName("OTHER");
-            c.authenticationEntryPoint(new CustomEntryPoint());
-        });
+//    private final CustomAuthenticationSuccessHandler authenticationSuccessHandler;
+//    private final CustomAuthenticationFailureHandler authenticationFailureHandler;
+//
+//    public ProjectConfig(CustomAuthenticationSuccessHandler authenticationSuccessHandler, CustomAuthenticationFailureHandler authenticationFailureHandler) {
+//        this.authenticationSuccessHandler = authenticationSuccessHandler;
+//        this.authenticationFailureHandler = authenticationFailureHandler;
+//    }
 
-        http.authorizeRequests().anyRequest().authenticated();
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .defaultSuccessUrl("/home", true)
+                .and()
+            .build();
     }
 
     @Bean
--- src/main/java/com/sudang/myspringsecurity/controller/HelloController.java
+++ src/main/java/com/sudang/myspringsecurity/controller/HelloController.java
@@ -1,68 +1,17 @@
 package com.sudang.myspringsecurity.controller;
 
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
-import org.springframework.security.concurrent.DelegatingSecurityContextExecutorService;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * @author sudang
  * @date   2023/05/19
 **/
-@RestController
-@Async
+@Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(Authentication authentication) {
-        return "Hello, " + authentication.getName() + "!";
-    }
-
-    @GetMapping("/ciao")
-    public String ciao() {
-        Callable<String> task = () -> {
-            SecurityContext securityContext = SecurityContextHolder.getContext();
-            return securityContext.getAuthentication().getName();
-        };
-
-        // ExecutorService 는 Callable 을 Job 으로 등록하고 수행시킬 수 있음 -> Thread pool 관련
-        ExecutorService e = Executors.newCachedThreadPool();
-        try {
-            // SecurityContextHolder 의 MODE 설정 후 사용
-            var contextTask = new DelegatingSecurityContextCallable<>(task);
-            return "Ciao, " + e.submit(contextTask).get() + "!";
-        } catch (ExecutionException | InterruptedException ex) {
-            throw new RuntimeException(ex);
-        } finally {
-            e.shutdown();
-        }
-    }
-
-    @GetMapping("/hola")
-    public String hola() {
-        Callable<String> task = () -> {
-            SecurityContext securityContext = SecurityContextHolder.getContext();
-            return securityContext.getAuthentication().getName();
-        };
-
-        ExecutorService e = Executors.newCachedThreadPool();
-        // Thread pool 에 작업을 제출하는 방식
-        e = new DelegatingSecurityContextExecutorService(e);
-        try {
-            return "Hola, " + e.submit(task).get() + "!";
-        } catch (ExecutionException | InterruptedException ex) {
-            throw new RuntimeException(ex);
-        } finally {
-            e.shutdown();
-        }
+    @GetMapping("/home")
+    public String home() {
+        return "home.html";
     }
 }
