d8d41bb9a134a82c816e54eb0f091bc8f58d2eee
sudang <tidh1640@naver.com>
2023-06-20 15:59:37 +0900

    Feat✨: 7장 실습 - 권한과 역할의 차이 실습

--- src/main/java/com/sudang/myspringsecurity/config/ProjectConfig.java
+++ src/main/java/com/sudang/myspringsecurity/config/ProjectConfig.java
@@ -21,11 +21,14 @@ public class ProjectConfig {
 
         var user1 = User.withUsername("yang")
                 .password("1234")
-                .authorities("READ")
+                .roles("ADMIN")
+                // 권한 메서드로 역할을 넣으려면 'ROLE_'을 붙여야 함.
+                // .authorities("ROLE_ADMIN")
                 .build();
         var user2 = User.withUsername("achi")
                 .password("1234")
-                .authorities("WRITE")
+                .roles("MANAGER")
+                // .authorities("ROLE_MANAGER")
                 .build();
 
         manager.createUser(user1);
@@ -39,19 +42,12 @@ public class ProjectConfig {
         return NoOpPasswordEncoder.getInstance();
     }
 
-    // 권한 부여 구성 추가
-    // WebSecurityConfigurerAdapter 가 Deprecated 된 후 사용되는 방법
-    // formLogin 방식과 httpBasic 방식 존재
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.httpBasic();
 
         http.authorizeRequests()
-                .anyRequest().hasAuthority("WRITE");
-                // 위 코드와 동일하게 동작하지만 spEL을 사용하므로 구문이 복잡해짐.
-                // .anyRequest().access("hasAuthority('WRITE')");
-                // access()는 hasAuthority(), hasAnyAuthority()를 사용할 수 없거나 아래와 같이 범용적인 권한 부여가 필요할 때만 사용함.
-                // .anyRequest().access("hasAuthority('READ') and !hasAuthority('delete')");
+                .anyRequest().hasRole("ADMIN");
 
         return http.build();
     }
