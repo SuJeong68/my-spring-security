d4540938c9763be590813334b6a0214a0b6e967b
sudang <tidh1640@naver.com>
2023-06-22 16:58:04 +0900

    Feat✨: 10장 실습 - CSRF 적용

--- src/main/java/com/sudang/myspringsecurity/config/ProjectConfig.java
+++ src/main/java/com/sudang/myspringsecurity/config/ProjectConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CsrfFilter;
 
@@ -13,12 +18,34 @@ import org.springframework.security.web.csrf.CsrfFilter;
 public class ProjectConfig {
 
     @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailsManager = new InMemoryUserDetailsManager();
+
+        var user1 = User.withUsername("yang")
+                .password("1234")
+                .authorities("READ")
+                .build();
+
+        userDetailsManager.createUser(user1);
+
+        return userDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.httpBasic();
 
         http.addFilterAfter(new CsrfTokenLogger(), CsrfFilter.class)
                 .authorizeRequests()
-                .anyRequest().permitAll();
+                .anyRequest().authenticated();
+
+        http.formLogin()
+                .defaultSuccessUrl("/main", true);
 
         return http.build();
     }
